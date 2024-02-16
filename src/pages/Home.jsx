7b1a012c60cae2d13ae63a830eeb716ad3a065bb
Madhur1808 <77977225+Madhur1808@users.noreply.github.com>
2024-02-16 13:04:10 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,130 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Card,
+  Typography,
+  Box,
+  Button,
+  CardContent,
+  TextField,
+} from "@mui/material";
+
+const Home = () => {
+  // api call
+  const [symbols, setSymbols] = useState([]);
+
+  const [error, setError] = useState(null);
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredSymbols, setFilteredSymbols] = useState([]);
+
+  const apiUrl = "https://finnhub.io/api/v1/stock/symbol";
+  const apiKey = "cn7fsa9r01qgjtj4jbu0cn7fsa9r01qgjtj4jbug";
+
+  useEffect(() => {
+    // Making a GET request using Axios within the useEffect hook
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(apiUrl, {
+          params: {
+            exchange: "US",
+            token: apiKey,
+          },
+        });
+
+        // Extracting symbols from the response data
+        // const symbolData = response?.data?.map((company) => company?.symbol);
+        const symbolData = response?.data?.slice(0, 300);
+
+        setSymbols(symbolData);
+
+        // console.log(symbols);
+        console.log(response?.data[0]);
+      } catch (error) {
+        console.error("Error:", error);
+        setError("An error occurred while fetching data.");
+      }
+    };
+
+    fetchData();
+  }, []); // The empty dependency array ensures that
+
+  useEffect(() => {
+    // Update filteredSymbols based on the search query
+    setFilteredSymbols(
+      symbols.filter((symbol) =>
+        symbol.symbol.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    );
+  }, [searchQuery, symbols]);
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  //   // Debounce the search function
+  //   const debouncedSearch = debounce((query) => {
+  //     setSearchQuery(query);
+  //   }, 300);
+
+  //   const handleSearch = (event) => {
+  //     debouncedSearch(event.target.value);
+  //   };
+
+  //   const filteredSymbols = symbols.filter((symbol) =>
+  //     symbol.symbol.toLowerCase().includes(searchQuery.toLowerCase())
+  //   );
+
+  return (
+    // <div></div>
+    <div>
+      {error ? (
+        <p>Error: {error}</p>
+      ) : symbols.length > 0 ? (
+        <div
+          style={{
+            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {/* Displaying Material-UI cards with stock symbols */}
+          <Typography
+            style={{ fontSize: "20px", fontWeight: 700, margin: "20px" }}
+          >
+            Data of different companies.
+          </Typography>
+          <TextField
+            type="text"
+            placeholder="Search by symbol"
+            onChange={handleSearch}
+            style={{ width: "500px" }}
+          />
+          <Box style={{ marginTop: "20px" }}>
+            {filteredSymbols.map((symbolCard, index) => (
+              <Card key={index} variant="outlined" style={{ marginBottom: 10 }}>
+                <CardContent>
+                  <Typography variant="h6" component="div">
+                    Stock Symbol: {symbolCard.symbol}
+                  </Typography>
+                  <Typography variant="h6" component="div">
+                    Currency: {symbolCard.currency}
+                  </Typography>
+                  <Typography variant="h6" component="div">
+                    Description: {symbolCard.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            ))}
+          </Box>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default Home;
